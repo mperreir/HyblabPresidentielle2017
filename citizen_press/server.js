62ed26f5f910ef3ae105d7f96fcfd84bf7657fd7
Aurélien Gauvrit <aurelien.gauvrit@etu.univ-nantes.fr>
2017-03-17 09:37:17 +0100

    Ready pour récuperation master

--- citizen_press/server.js
+++ citizen_press/server.js
@@ -10,8 +10,49 @@ var app = express();
 var fs = require('fs');
 
 app.get("/", (req, res) => {
-	console.log("Page principale");
+	
 	res.send();
+});
+
+app.get("/test", (req,res) => {
+
+	res.set({"Content-Type" : "text/html"});
+	
+	var body = "";
+	fs.readFile('citizen_press/public/html/header.html','utf8', function(err,data){	// Lecture d'un fichier
+		body += data;	// Ecriture dans la réponse
+	});	 
+
+	console.log(body);
+	// Préparation du parsage JSON pour la création des éléments
+	fs.readFile('citizen_press/public/data/data.json', 'utf8', function (err, data) {
+	    if (err) throw err; // à voir 
+	    var obj = JSON.parse(data);
+	    //res.contentType('json');
+	    var tab = [];
+	      
+	    for(var i=0; i<=obj.bureaux.length-1; i++){
+	    	if (tab.indexOf(obj.bureaux[i].adresse) == -1){
+		    	body += '<section class="POI'+i+1+'">\
+		    				<div class="bureaux"></div>\
+	    					<div class="fermer"></div> \
+        					<div class="header"> \
+        						<div class="adress"></div>\
+        						<div class="data-container"></div> \
+        					</div>\
+        					<div class="other"></div> \
+            			</section> \n';
+            	tab.push(obj.bureaux[i].adresse);
+	    	};
+		};
+
+		fs.readFile('citizen_press/html/footer.html','utf8', function(err,data){	// Lecture d'un fichier
+			body += data;	// Ecriture dans la réponse
+		});
+
+		res.write(body);
+		res.end();
+	});
 });
 
 // GET bureaux (pour la map)
@@ -76,3 +117,22 @@ app.get("/assesseurs", (req, res) => {
 // server and visiting http(s)://127.0.0.1:8080/name_of_you_project/ (if on a local server)
 // or more generally: http(s)://server_name:port/name_of_you_project/
 module.exports = app;
+
+
+
+// Définition des variables à donner au template
+	    //var taille_liste_bureaux = obj.bureaux.length;
+	    //res.render('test.ejs', {objPrincipal: obj});
+	    /*for (var bureau in obj.bureaux) {
+	    	res.write(JSON.stringify(obj.bureaux[bureau]));
+	    }
+	    res.send();
+	});
+	//res.send();
+
+	/*var l_s = "Ecole primaire";
+	var l_a = "18 rue des blabla";
+	var le_num_b = 1;
+	var tab = [1,2,3,4];
+	res.render('test.ejs', {lieu_site: l_s, lieu_adresse: l_a, taille_liste: tab.length, liste_bureau: tab,
+	le_num_bureau: le_num_b});*/
