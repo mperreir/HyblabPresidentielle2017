edf104139de0b46fbffccd84b8502e3d09f206de
Tom Marrucci <tom.marrucci@etu.univ-nantes.fr>
2017-03-19 17:12:53 +0100

    debut svg generation

--- citizen_press/server.js
+++ citizen_press/server.js
@@ -11,9 +11,66 @@ var app = express();
 var fs = require('fs');
 
 var URL_DATA = 'citizen_press/public/data/data2.json';
+var NB_MAX_ASSESSEURS = 8;
 
 // Recuperation des chemins relatifs
 app.use(express.static(path.join(__dirname, 'public')));  
+
+// Écrit un SVG à partir des bureaux entré en entrée
+// Les bureaux sont à la même adresse (un seul POI)
+// Retourne l'URL du fichier créé
+function writeSvg(bureaux, numPOI) {	
+	var color;
+	var colors = new Array();
+	var tauxRemplissage;	
+	var nbAssesseursMin = NB_MAX_ASSESSEURS;
+	var svg = new SVG(numPOI);
+	console.log(svg.url);
+	console.log(bureaux);
+
+	// Petits cercles
+	bureaux.forEach(function(element, key) {
+		if (nbAssesseursMin > element){
+			nbAssesseursMin = element;
+		}
+		tauxRemplissage = element/NB_MAX_ASSESSEURS;
+		if (tauxRemplissage < 0.4) {
+			color = "red";
+			colors.push("red");
+		}
+		else if ((tauxRemplissage >= 0.4)&&(tauxRemplissage < 0.7)) {
+			color = "orange";
+			colors.push("orange");
+		}
+		else if ((tauxRemplissage >= 0.7)&&(tauxRemplissage < 1)) {
+			color = "yellow";
+			colors.push("yellow");
+		}
+		else if (tauxRemplissage == 1) {
+			color = "green";
+			colors.push("green");
+		}
+		svg.addPoint(color);
+	});
+	// Gros cercle
+	if (colors.indexOf("red") != -1) {
+		svg.setBigCircle("red");
+	}
+	else if (colors.indexOf("orange") != -1) {
+		svg.setBigCircle("orange");
+	}
+	else if (colors.indexOf("yellow") != -1) {
+		svg.setBigCircle("yellow");
+	}
+	else {
+		svg.setBigCircle("green");
+	}
+
+	// Nombre à l'intérieur
+	svg.setNumber(nbAssesseursMin);
+
+	fs.writeFile("./citizen_press/public/img/"/svg.url, svg.getContent());
+}
 
 // Route d'accès client
 app.get("/", (req, res) => {
@@ -56,6 +113,10 @@ app.get("/", (req, res) => {
 	});
 });
 
+app.get("/test", (req, res) => {
+	res.sendFile(path.join(__dirname, '../citizen_press/public', 'html/test.html'));
+});	
+
 // GET bureaux (pour la map)
 app.get("/bureaux", (req, res) => {
 	console.log("Chargement des bureaux...");
@@ -81,6 +142,42 @@ app.get("/bureaux/:id", (req, res) => {
 	    	}
 	    }
 	    res.send();
+	});
+});
+
+// Récupère les données des bureaux à l'adresse :adresse pour poouvoir renvoyer le bon SVG
+app.get("/bureaux/:adresse/:numPOI", (req, res) => {
+	var nbAssesseursInscrit = 0;
+	var bureaux = new Map();
+	var bureau;
+	var adresse = req.params.adresse;
+	var numPOI = req.params.numPOI;
+	fs.readFile(URL_DATA, 'utf8', function(err, data) {
+	    if (err) throw err; // à voir 
+	    var obj = JSON.parse(data);
+	   // console.log(obj.bureaux);
+	    // Parcours des bureaux
+	    for (var bureauIndex in obj.bureaux) {
+	    	// Selection des bons bureaux à la même adresse
+	    	bureau = obj.bureaux[bureauIndex];
+	    	//console.log(adresse);
+	    	if (bureau.adresse == adresse) {
+	    		// Parcours des assesseurs
+	    		for (var assesseur in bureau.assesseurs) {
+	    			// Si assesseur inscrit on augmente le compteur
+	    			if (bureau.assesseurs[assesseur].valide_assesseur) {
+	    				nbAssesseursInscrit++;
+	    			}
+	    		}
+	    		// Ajout du nombre d'assesseur inscrit par rapport au bureaum=
+	    		bureaux.set(bureau.id, nbAssesseursInscrit);
+	    		// Resmise à 0 du nombre d'inscrit
+	    		nbAssesseursInscrit = 0;
+	    	}
+	    }
+	    // On envoie l'URL du fichier créé
+	   // console.log(bureaux);
+	    res.send(writeSvg(bureaux, numPOI));
 	});
 });
 
@@ -111,14 +208,77 @@ app.get("/connexion", (req, res) => {
 
 // La page des assesseurs validé sur un bureau
 app.get("/bureaux/:id/assesseurs", (req, res) => {
- // TODO
+
 });
 
 // La page des statistique globales sur les assesseurs
-app.get("/	assesseurs", (req, res) => {
+app.get("/assesseurs", (req, res) => {
  // TODO
 });
 
+/*
+ * Fichier pour l'accès à l'image SVG
+ * Permet ses modifications :
+ 	* L'ajout de point,
+ 	* La modification des couleurs
+ 	* La modification du chiffre
+*/
+
+function SVG(numPOI) {
+	this.url = "svg"+numPOI+".svg";
+	this.contentSVG_littleCircles = "";
+	this.nbBureau = 0;
+
+	// Init du SVG
+	this.contentSVG_init = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+		viewBox="0 0 1350 1360" style="enable-background:new 0 0 0 0;" xml:space="preserve">\
+		<style type="text/css">\
+			.red{fill:#EE5A58;}\
+		    .orange {fill:#F1A72E;}\
+		    .green{fill:#AED17C;}\
+			.yellow{fill:#F4D05D;}\
+			.st2{fill:#FFFFFF;}\
+			.st3{font-family:Lato-Bold, Lato;font-weight:700;}\
+			.st4{font-size:46.3393px;}\
+		</style>';
+	
+
+	// Ajoute le cercle principale 
+	// classColor : la classe à ajouter
+	this.setBigCircle = function (classColor) {
+		this.contentSVG_bigCircle = '<circle class="'+classColor+'" cx="66.7" cy="95.9" r="63.4"/>';
+	}
+
+	this.addPoint = function(classColor) {
+		this.nbBureau++;
+		if (this.nbBureau == 1) {
+			this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="66.7" cy="12.9" r="12.4"/>';
+		}
+		else if (this.nbBureau == 2) {
+			this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="105.4" cy="21.1" r="12.4"/>';
+		}
+		else if (this.nbBureau == 3) {
+			this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="134.4" cy="44.3" r="12.4"/>';
+		}
+		else if (this.nbBureau == 4) {
+			this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="150.4" cy="78.1" r="12.4"/>';
+		}
+	}
+
+	this.setNumber = function(number) {
+		if (number > 9) {
+			this.contentSVG_text = '<text transform="matrix(1 0 0 1 37.9507 113.4561)" class="st2 st3 st4">'+number+'</text>';
+		}
+		else {
+			this.contentSVG_text = '<text transform="matrix(1 0 0 1 37.9507 113.4561)" class="st2 st3 st4">0'+number+'</text>';
+		}
+	}
+
+	this.getContent = function() {
+		return this.contentSVG_init+this.contentSVG_bigCircle+this.contentSVG_littleCircles+this.contentSVG_text+"</svg>";
+	}
+
+}
 
 // Minimum routing: serve static content from the html directory
 //app.use(express.static(path.join(__dirname, 'public')));
