9b19325201dd7fe21a7921f2caae32afde78f90e
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2017-03-19 17:12:52 +0100

    Recup bureau avec id, recup infos bureaux et infos assesseurs, PB : Affiche les assesseurs en dehors de la table

--- citizen_press/public/js/liste_president.js
+++ citizen_press/public/js/liste_president.js
@@ -1,16 +1,3 @@
-/*$(document).ready(function() {
-    $('#table_benevoles').DataTable( {
-        "language": {
-            "lengthMenu": "Afficher _MENU_ bénévoles par page",
-            "zeroRecords": "Nous n'avons trouvé aucune demande de bénévoles, désolé",
-            "info": "Page _PAGE_ sur _PAGES_",
-            "infoEmpty": "Pas de bénévoles disponibles",
-            "infoFiltered": "(Afficher _MAX_ bénévoles par pages)"
-        }
-    } );
-} );
-*/
-
 $(document).ready(function() {
     $('#table_benevoles').DataTable( {
         "scrollY":        "200px",
@@ -22,3 +9,5 @@ $(document).ready(function() {
       }
     } );
 } );
+
+//Requete Ajax
--- citizen_press/server.js
+++ citizen_press/server.js
@@ -16,48 +16,95 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route d'accès client
 app.get("/", (req, res) => {
 
-	res.set({"Content-Type" : "text/html"});
-	
-	// Initialisation de la page renvoyé
-	var body = "";
+ 	res.set({"Content-Type" : "text/html"});
 
 	// Récupération du header de la page
-	fs.readFile('citizen_press/public/html/header.html','utf8', function(err,data){	// Lecture d'un fichier
-		body += data;	// Ecriture dans la réponse
-	});	 
+	fs.readFile('citizen_press/public/html/liste_president/header.html','utf8', function(err,data){	// Lecture d'un fichier
+		res.write(data);
+	// Ecriture dans la réponse
+	});
+
+	 
 
 	// Préparation du parsage JSON pour la création des éléments
-	fs.readFile('citizen_press/public/data/data.json', 'utf8', function (err, data) {
+	fs.readFile('citizen_press/public/data/data2.json', 'utf8', function (err, data) {
 	    if (err) throw err; // à voir 
 	    var obj = JSON.parse(data);
 	   
+	    //var id = req.params.id;
+	    var idBureau = 513;
+
 	   	// Initialisation des variables
-	    var tab = [];
-	    var calc = 0;
-	      
+	    var assesseurs = [];
+	    var asseseurTemp;
 	    // Parcours des bureaux pour création de points d'intêrets
-	    for(var i=0; i<=obj.bureaux.length-1; i++){
-	    	if (tab.indexOf(obj.bureaux[i].adresse) == -1){
-		    	body += '<section class="POI'+calc+'">\
-		    				<div class="bureaux"></div>\
-        					<img class="fermer" src="arrow.png"> \
-        					<div class="data-container"></div> \
-        					<div class="other"></div> \
-            			</section> \n';
-            	calc++;
-            	tab.push(obj.bureaux[i].adresse);
-	    	};
+	    for(var index in obj.bureaux){
+	    	if (obj.bureaux[index].id == idBureau){
+	    		res.write("<div class=\"resume_buro\"><p class = \"nom_lieu_adresse\">");
+	    		res.write(obj.bureaux[index].nom_lieu + "<br>" + obj.bureaux[index].adresse);
+        		res.write("</p><p class = \"codepo_ville\" >");
+	    		res.write(obj.bureaux[index].code_postal + " " + obj.bureaux[index].ville);
+        		res.write("</p></div>");
+        		for(var index2 in obj.bureaux[index].assesseurs){
+        			asseseurTemp = obj.bureaux[index].assesseurs[index2];
+        			assesseurs.push([asseseurTemp.id,asseseurTemp.valide_assesseur,asseseurTemp.valide_scrutateur]);
+        		}
+	    	}
 		};
 
-		// Récupération du footer
-		fs.readFile('citizen_press/html/footer.html','utf8', function(err,data){	// Lecture d'un fichier
-			body += data;	// Ecriture dans la réponse
+		fs.readFile('citizen_press/public/html/liste_president/filtrage.html','utf8', function(err,data){	// Lecture d'un fichier
+			res.write(data);
+		// Ecriture dans la réponse
 		});
 
-		// Ecriture et envoi de la réponse
-		res.write(body);
-		res.end();
+
+		for(var index3 in assesseurs){
+			for (var index4 in obj.assesseurs){
+				if (obj.assesseurs[index4].id == assesseurs[index3][0]){
+					//Assesseur en cours
+					asseseurTemp = obj.assesseurs[index4];
+					/*if (asseseurTemp.potentiel_assesseur == true && asseseurTemp.potentiel_scrutateur == true ){
+						res.write("<td class=\"assesseurs\" class=\"scrutateur\"");
+					}else if (asseseurTemp.potentiel_assesseur == true)
+						res.write("<td class=\"assesseurs\" ");
+					}else if (asseseurTemp.potentiel_scrutateur == true)
+						res.write("<td class=\"scrutateur\" ");
+					}*/
+					res.write("<tr ");
+					if (asseseurTemp.potentiel_assesseur == true ){
+						if (assesseurs[index3][0].valide_assesseur == true){
+							res.write("class=\"assesseur_valide\" ");
+						}else{
+							res.write("class=\"assesseur_non_valide\" ");
+						}
+					}
+					if (asseseurTemp.potentiel_scrutateur == true ){
+						if (assesseurs[index3][0].valide_scrutateur == true){
+							res.write("class=\"scrutateur_valide\" ");
+						}else{
+							res.write("class=\"scrutateur_non_valide\" ");
+						}
+					}
+					res.write(">");
+					res.write("<td>"+asseseurTemp.nom.toUpperCase()+" "+asseseurTemp.prenom+"</td>");
+					res.write("<td>"+asseseurTemp.age+"</td>");
+					res.write("<td>"+asseseurTemp.mail+"</td>");
+					res.write("<td>"+asseseurTemp.tel+"</td>");
+					res.write("</tr>");
+				}
+			}
+		}
+		res.write("</tbody></table>");
+
+
+
+		// Récupération du footer
+		fs.readFile('citizen_press/public/html/liste_president/footer.html','utf8', function(err,data){	// Lecture d'un fichier
+			res.write(data);	// Ecriture dans la réponse
+			res.end();
+		});
 	});
+
 });
 
 app.get("/test", (req,res) => {
@@ -105,6 +152,7 @@ app.get("/bureaux/:id", (req, res) => {
 // La page du formulaire d'inscription
 app.get("/bureaux/:id/inscription" , (req, res) => {
 
+
 });
 
 // Ajout assesseurs
@@ -129,7 +177,7 @@ app.get("/connexion", (req, res) => {
 
 // La page des assesseurs validé sur un bureau
 app.get("/bureaux/:id/assesseurs", (req, res) => {
- // TODO
+
 });
 
 // La page des statistique globales sur les assesseurs
