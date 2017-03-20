440dc06ac8d04d26944a47bf5afd5d63813d7978
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2017-03-20 17:53:04 +0100

    Filtrage Ok, Cohérence Validé et boutons valider/refuser ok - Faire actions valider et refuser

--- citizen_press/public/js/liste_president.js
+++ citizen_press/public/js/liste_president.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $(".assesseur_valide").hide();
     $(".scrutateur_valide").hide();
     $(".scrutateur_non_valide").hide();
+    $(".assesseur_non_valide").toggle(true);
 
     $('input[type=radio]').change(function(){
         isAssesseur = document.getElementById('assesseurs_filtre').checked;
@@ -12,42 +13,62 @@ $(document).ready(function() {
         isEnCours = document.getElementById('en_cours_filtre').checked;
         isValide = document.getElementById('valide_filtre').checked;
 
+        //En plus de cacher les lignes ou non en fonction de ce qui est demandé
+        // Il faut modifier la case décision en fonction des diiférents cas, (2 demandes, 1 seule validée)
+        // cf. Tableau "Liste président : Que afficher dans la case décision en fonction des différents cas et du filtrage demandé" sur le Google Drive des Dépouilleurs
+        // 
         //Code optimisable via les paramètre de toggle et hide (pas le temps d'y réflechir)
+
         if(isAssesseur && isEnCours){
             $(".assesseur_valide").hide();
             $(".scrutateur_valide").hide();
             $(".scrutateur_non_valide").hide();
-            $(".assesseur_non_valide").toggle();
+            $(".assesseur_non_valide").toggle(true);
+            //cf.Tableau
+            $(".assesseur_non_valide .decision").replaceWith('<td class="decision"><input type="button" id="ValiderAss" value="Valider"><input type="button" id="RefuserAss" value="Refuser"></td>');
+
         }else if (isAssesseur && isValide){
             $(".scrutateur_valide").hide();
             $(".scrutateur_non_valide").hide();
             $(".assesseur_non_valide").hide();
-            $(".assesseur_valide").toggle();
+            $(".assesseur_valide").toggle(true);
+            //cf. Tableau
+            $(".assesseur_valide .decision").replaceWith("<td class=\"decision\">Validé</td>");
+
         }else if(isScrutateur && isEnCours){
             $(".scrutateur_valide").hide();
             $(".assesseur_non_valide").hide();
             $(".assesseur_valide").hide();
-            $(".scrutateur_non_valide").toggle();
+            $(".scrutateur_non_valide").toggle(true);
+            //cf. Tableau
+            $(".scrutateur_non_valide .decision").replaceWith('<td class="decision"><input type="button" id="ValiderScrut" value="Valider"><input type="button" id="RefuserScrut" value="Refuser"></td>');
+
         }else if (isScrutateur && isValide){
             $(".assesseur_non_valide").hide();
             $(".assesseur_valide").hide();
             $(".scrutateur_non_valide").hide();
-            $(".scrutateur_valide").toggle();
+            $(".scrutateur_valide").toggle(true);
+            //cf. Tableau
+            $(".scrutateur_valide .decision").replaceWith('<td class="decision">Validé</td>');
         }            
     });
 
+    miseEnFormeTable();
 
 
     //Mise en forme de la table et du tri
-    $('#table_benevoles').DataTable( {
-        "scrollY":        "200px",
-        "scrollCollapse": true,
-        "paging":         false,
-        "language": {
-          "info":           "",//On affiche rien au lieu de "_TOTAL_ bénévoles affichés au total"
-          "search":         "Rechercher:"
-      }
-    } );
+    function miseEnFormeTable(){
+        $('#table_benevoles').DataTable( {
+            "scrollY":        "200px",
+            "scrollCollapse": true,
+            "paging":         false,
+            "language": {
+              "info":           "",//On affiche rien au lieu de "_TOTAL_ bénévoles affichés au total"
+              "search":         "Rechercher:"
+          }
+        } );
+    }
+
 } );
 
 //Requete Ajax
--- citizen_press/server.js
+++ citizen_press/server.js
@@ -16,105 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route d'accès client
 app.get("/", (req, res) => {
 
- 	res.set({"Content-Type" : "text/html"});
-
-	// Récupération du header de la page
-	fs.readFile('citizen_press/public/html/liste_president/header.html','utf8', function(err,data){	// Lecture d'un fichier
-		res.write(data);
-	// Ecriture dans la réponse
-	});
-
-	 
-
-	// Préparation du parsage JSON pour la création des éléments
-	fs.readFile('citizen_press/public/data/data2.json', 'utf8', function (err, data) {
-	    if (err) throw err; // à voir 
-	    var obj = JSON.parse(data);
-	   
-	    //var id = req.params.id;
-	    var idBureau = 133;
-
-	   	// Initialisation des variables
-	    var assesseurs = [];
-	    var asseseurTemp;
-	    // Parcours des bureaux pour création de points d'intêrets
-	    for(var index in obj.bureaux){
-	    	if (obj.bureaux[index].id == idBureau){
-	    		res.write("<div class=\"resume_buro\"><p class = \"nom_lieu_adresse\">");
-	    		res.write(obj.bureaux[index].nom_lieu + "<br>" + obj.bureaux[index].adresse);
-        		res.write("</p><p class = \"codepo_ville\" >");
-	    		res.write(obj.bureaux[index].code_postal + " " + obj.bureaux[index].ville);
-        		res.write("</p></div>");
-        		for(var index2 in obj.bureaux[index].assesseurs){
-        			asseseurTemp = obj.bureaux[index].assesseurs[index2];
-        			assesseurs.push([asseseurTemp.id,asseseurTemp.valide_assesseur,asseseurTemp.valide_scrutateur]);
-        		}
-	    	}
-		};
-
-		res.write('<div class="Filtrage">');
-        res.write('FILTRAGE : ');
-        res.write('<form action="">');
-  		res.write('<input type="radio" class="type_benevole" name="type_benevole" id="assesseurs_filtre" checked> Assesseurs<br>');
-  		res.write('<input type="radio" class="type_benevole" name="type_benevole" id="scrutateurs_filtre"> Scrutateurs<br>');
-		res.write('</form>');
-		res.write('<br>');
-		res.write('<form action="">');
-		res.write('<input type="radio" class="type_demande" name="type_demande" id="en_cours_filtre" checked> Demandes en cours : <br>');
-		res.write('<input type="radio" class="type_demande "name="type_demande" id="valide_filtre"> Demandes validées : <br>');
-		res.write('</form>');
-		res.write('</div>');
-		res.write('<br><br><br><br>');
-		res.write('<table id="table_benevoles" class="display" cellspacing="0" width="100%">');
-		res.write('<thead>');
-	    res.write('<tr>');
-	    res.write('<th>NOM Prénom</th>');
-	    res.write('<th>Age</th>');
-	    res.write('<th>Email</th>');
-      	res.write('<th>Téléphone</th>');
-      	res.write('<th>Décision</th>');
-    	res.write('</tr>');
-  		res.write('</thead>');
- 		res.write(' <tbody>');
-
-		for(var index3 in assesseurs){
-			for (var index4 in obj.assesseurs){
-				if (obj.assesseurs[index4].id == assesseurs[index3][0]){
-					//Assesseur en cours
-					asseseurTemp = obj.assesseurs[index4];
-					res.write("<tr ");
-					if (asseseurTemp.potentiel_assesseur == true ){
-						if (assesseurs[index3][0].valide_assesseur == true){
-							res.write("class=\"assesseur_valide\" ");
-						}else{
-							res.write("class=\"assesseur_non_valide\" ");
-						}
-					}
-					if (asseseurTemp.potentiel_scrutateur == true ){
-						if (assesseurs[index3][0].valide_scrutateur == true){
-							res.write("class=\"scrutateur_valide\" ");
-						}else{
-							res.write("class=\"scrutateur_non_valide\" ");
-						}
-					}
-					res.write(">");
-					res.write("<td>"+asseseurTemp.nom.toUpperCase()+" "+asseseurTemp.prenom+"</td>");
-					res.write("<td>"+asseseurTemp.age+"</td>");
-					res.write("<td>"+asseseurTemp.mail+"</td>");
-					res.write("<td>"+asseseurTemp.tel+"</td>");
-					res.write('<td><input type="button" value="Valider"><input type="button" value="Refuser"></td></tr>');
-					res.write("</tr>");
-				}
-			}
-		}
-		res.write("</tbody></table>");
-
-		// Récupération du footer
-		fs.readFile('citizen_press/public/html/liste_president/footer.html','utf8', function(err,data){	// Lecture d'un fichier
-			res.write(data);	// Ecriture dans la réponse
-			res.end();
-		});
-	});
 
 });
 
@@ -189,6 +90,123 @@ app.get("/connexion", (req, res) => {
 // La page des assesseurs validé sur un bureau
 app.get("/bureaux/:id/assesseurs", (req, res) => {
 
+ 	res.set({"Content-Type" : "text/html"});
+
+	// Récupération du header de la page
+	fs.readFile('citizen_press/public/html/liste_president/header.html','utf8', function(err,data){	// Lecture d'un fichier
+		res.write(data);
+	// Ecriture dans la réponse
+	});
+
+	 
+
+	// Préparation du parsage JSON pour la création des éléments
+	fs.readFile('citizen_press/public/data/data2.json', 'utf8', function (err, data) {
+	    if (err) throw err; // à voir 
+	    var obj = JSON.parse(data);
+	   
+	    //var id = req.params.id;
+	    var idBureau = req.params.id;
+
+	   	// Initialisation des variables
+	    var assesseurs = [];
+	    var asseseurTemp;
+	    // Parcours des bureaux pour création de points d'intêrets
+	    for(var index in obj.bureaux){
+	    	if (obj.bureaux[index].id == idBureau){
+	    		res.write("<div class=\"resume_buro\"><p class = \"nom_lieu_adresse\">");
+	    		res.write(obj.bureaux[index].nom_lieu + "<br>" + obj.bureaux[index].adresse);
+        		res.write("</p><p class = \"codepo_ville\" >");
+	    		res.write(obj.bureaux[index].code_postal + " " + obj.bureaux[index].ville);
+        		res.write("</p></div>");
+        		for(var index2 in obj.bureaux[index].assesseurs){
+        			asseseurTemp = obj.bureaux[index].assesseurs[index2];
+        			assesseurs.push([asseseurTemp.id,asseseurTemp.valide_assesseur,asseseurTemp.valide_scrutateur]);
+        		}
+	    	}
+		};
+
+		res.write('<div class="Filtrage">');
+        res.write('FILTRAGE : ');
+        res.write('<form action="">');
+  		res.write('<input type="radio" class="type_benevole" name="type_benevole" id="assesseurs_filtre" checked> Assesseurs<br>');
+  		res.write('<input type="radio" class="type_benevole" name="type_benevole" id="scrutateurs_filtre"> Scrutateurs<br>');
+		res.write('</form>');
+		res.write('<br>');
+		res.write('<form action="">');
+		res.write('<input type="radio" class="type_demande" name="type_demande" id="en_cours_filtre" checked> Demandes en cours : <br>');
+		res.write('<input type="radio" class="type_demande "name="type_demande" id="valide_filtre"> Demandes validées : <br>');
+		res.write('</form>');
+		res.write('</div>');
+		res.write('<br><br><br><br>');
+		res.write('<table id="table_benevoles" class="display" cellspacing="0" width="100%">');
+		res.write('<thead>');
+	    res.write('<tr>');
+	    res.write('<th>NOM Prénom</th>');
+	    res.write('<th>Age</th>');
+	    res.write('<th>Email</th>');
+      	res.write('<th>Téléphone</th>');
+      	res.write('<th>Décision</th>');
+    	res.write('</tr>');
+  		res.write('</thead>');
+ 		res.write(' <tbody>');
+
+
+		for(var index3 in assesseurs){
+			for (var index4 in obj.assesseurs){
+				if (obj.assesseurs[index4].id == assesseurs[index3][0]){
+					//Assesseur en cours
+					asseseurTemp = obj.assesseurs[index4];
+					res.write("<tr ");
+						//Asseseur et scrutateur
+						if (asseseurTemp.potentiel_assesseur && asseseurTemp.potentiel_scrutateur){
+							if (assesseurs[index3][1]){//Assesseur valide
+								res.write("class=\"assesseur_valide");
+							}else{
+								res.write("class=\"assesseur_non_valide");
+							}
+							if (assesseurs[index3][2]){//Scrutateur valide
+								res.write(" scrutateur_valide\"");
+							}else{
+								res.write(" scrutateur_non_valide\"");
+							}
+						}
+						//Juste scrutateur
+						else if (asseseurTemp.potentiel_assesseur){
+							console.log(assesseurs[index3][1]);
+							if (assesseurs[index3][1]){//Asseseurs valides
+								res.write("class=\"assesseur_valide\" ");
+							}else{ 
+								res.write("class=\"assesseur_non_valide\" ");
+							}
+						}
+						//Juste assesseur
+						else if (asseseurTemp.potentiel_scrutateur){
+							if (assesseurs[index3][2]){//Scrutateurs valides
+								res.write("class=\"scrutateur_valide\" ");
+							}else{
+								res.write("class=\"scrutateur_non_valide\" ");
+							}
+						}
+					res.write(">");
+					res.write("<td>"+asseseurTemp.nom.toUpperCase()+" "+asseseurTemp.prenom+"</td>");
+					res.write("<td>"+asseseurTemp.age+"</td>");
+					res.write("<td>"+asseseurTemp.mail+"</td>");
+					res.write("<td>"+asseseurTemp.tel+"</td>");
+					//Choix par défaut, au démarrage de la page : Assesseurs en cours
+					res.write('<td class="decision"><input type="button" id="ValiderAss" value="Valider"><input type="button" id="RefuserAss" value="Refuser"></td></tr>');
+					res.write("</tr>");
+				}
+			}
+		}
+		res.write("</tbody></table>");
+
+		// Récupération du footer
+		fs.readFile('citizen_press/public/html/liste_president/footer.html','utf8', function(err,data){	// Lecture d'un fichier
+			res.write(data);	// Ecriture dans la réponse
+			res.end();
+		});
+	});
 });
 
 // La page des statistique globales sur les assesseurs
