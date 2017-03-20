6265ad779e063712508f378aa057706355a0ab87
Tom Marrucci <tom.marrucci@etu.univ-nantes.fr>
2017-03-20 16:25:02 +0100

    test img svg

--- citizen_press/public/js/svg.js
+++ citizen_press/public/js/svg.js
@@ -6,68 +6,60 @@
  	* La modification du chiffre
 */
 
-// L'objet qui vas nous permettre de modifier le SVG
-// TOOD changer avec AJAX
-var svg = SVG('drawing').size(600, 600);
-
-var contentSVG_init, contentSVG_bigCircle, contentSVG_littleCircles, contentSVG_text;
-
-initialise();
-setBigCircle("red");
-addPoint1("green");
-addPoint2("yellow");
-addPoint3("orange");
-setNumber(2);
-show();
-console.log(svg.svg());
+var svg = function SVG(numPOI) {
+	this.url = "svg_"+numPOI+".svg":
 
 
-// Execute une requête pour avoir le contenu du fichier
-function initialise() {
-	contentSVG_init = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
-		viewBox="0 0 1350 1360" style="enable-background:new 0 0 0 0;" xml:space="preserve">\
-		<style type="text/css">\
-			.red{fill:#EE5A58;}\
-		    .orange {fill:#F1A72E;}\
-		    .green{fill:#AED17C;}\
-			.yellow{fill:#F4D05D;}\
-			.st2{fill:#FFFFFF;}\
-			.st3{font-family:Lato-Bold, Lato;font-weight:700;}\
-			.st4{font-size:46.3393px;}\
-		</style>';
-}
-
-// Ajoute le cercle principale 
-// classColor : la classe à ajouter
-function setBigCircle(classColor) {
-	contentSVG_bigCircle = '<circle class="'+classColor+'"cx="66.7" cy="95.9" r="63.4"/>';
-}
-
-function addPoint1(classColor) {
-	contentSVG_littleCircles = '<circle class="'+classColor+'" cx="66.7" cy="12.9" r="12.4"/>';
-}
-
-function addPoint2(classColor) {
-	contentSVG_littleCircles += '<circle class="'+classColor+'" cx="105.4" cy="21.1" r="12.4"/>';
-}
-
-function addPoint3(classColor) {
-	contentSVG_littleCircles += '<circle class="'+classColor+'" cx="134.4" cy="44.3" r="12.4"/>';
-}
-
-function addPoint4(classColor) {
-	contentSVG_littleCircles += '<circle class="'+classColor+'" cx="150.4" cy="78.1" r="12.4"/>';
-}
-
-function setNumber(number) {
-	if (number > 9) {
-		contentSVG_text = '<text transform="matrix(1 0 0 1 37.9507 113.4561)" class="st2 st3 st4">'+number+'</text>';
+	// Execute une requête pour avoir le contenu du fichier
+	this.initialise = function() {
+		this.contentSVG_init = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+			viewBox="0 0 1350 1360" style="enable-background:new 0 0 0 0;" xml:space="preserve">\
+			<style type="text/css">\
+				.red{fill:#EE5A58;}\
+			    .orange {fill:#F1A72E;}\
+			    .green{fill:#AED17C;}\
+				.yellow{fill:#F4D05D;}\
+				.st2{fill:#FFFFFF;}\
+				.st3{font-family:Lato-Bold, Lato;font-weight:700;}\
+				.st4{font-size:46.3393px;}\
+			</style>';
 	}
-	else {
-		contentSVG_text = '<text transform="matrix(1 0 0 1 37.9507 113.4561)" class="st2 st3 st4">0'+number+'</text>';
+
+	// Ajoute le cercle principale 
+	// classColor : la classe à ajouter
+	this.setBigCircle = function (classColor) {
+		this.contentSVG_bigCircle = '<circle class="'+classColor+'"cx="66.7" cy="95.9" r="63.4"/>';
+	}
+
+	this.addPoint1 = function(classColor) {
+		this.contentSVG_littleCircles = '<circle class="'+classColor+'" cx="66.7" cy="12.9" r="12.4"/>';
+	}
+
+	this.addPoint2 = function(classColor) {
+		this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="105.4" cy="21.1" r="12.4"/>';
+	}
+
+	this.addPoint3 = function(classColor) {
+		this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="134.4" cy="44.3" r="12.4"/>';
+	}
+
+	this.addPoint4 = function(classColor) {
+		this.contentSVG_littleCircles += '<circle class="'+classColor+'" cx="150.4" cy="78.1" r="12.4"/>';
+	}
+
+	this.setNumber = function(number) {
+		if (number > 9) {
+			this.contentSVG_text = '<text transform="matrix(1 0 0 1 37.9507 113.4561)" class="st2 st3 st4">'+number+'</text>';
+		}
+		else {
+			this.contentSVG_text = '<text transform="matrix(1 0 0 1 37.9507 113.4561)" class="st2 st3 st4">0'+number+'</text>';
+		}
+	}
+
+	this.getContent = function() {
+		return this.contentSVG_init+this.contentSVG_bigCircle+this.contentSVG_littleCircles+this.contentSVG_text+"</svg>";
 	}
 }
+module.exports.svg;
 
-function show() {
-	svg.svg(contentSVG_init+contentSVG_bigCircle+contentSVG_littleCircles+contentSVG_text);
-}
+
