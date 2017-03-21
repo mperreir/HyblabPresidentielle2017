9d0272c50b116936a9db6f4a79d3112b7dd7b37f
Tom Marrucci <tom.marrucci@etu.univ-nantes.fr>
2017-03-21 21:24:08 +0100

    checkbos ok

--- citizen_press/public/js/form.js
+++ citizen_press/public/js/form.js
@@ -1,20 +1,37 @@
-function addDays(selector){
-    var i;
-    selector.options[0] = new Option();
-    for (i=1;i<=31;i++){
-      selector.options[i] = new Option(i,i);
-    }
-}
+$(document).ready(function(){
 
-function addYears(selector){
-	var i;
-	var j=2017;
-    selector.options[0] = new Option();
-    for (i=1;i<=108;i++){
-      selector.options[i] = new Option(j,j);
-      j--;
-    }
-}
-//usage:
-addDays(document.getElementById("jour"));
-addYears(document.getElementById("annee"));
+	$("#check").change(function(){
+		if (this.checked) {
+			$("#check-bis").prop("checked", false);
+		}
+	});
+
+	$("#check-bis").change(function(){
+		if (this.checked) {
+			$("#check").prop("checked", false);
+		}
+	});
+
+	function addDays(selector){
+	  var i;
+	  selector.options[0] = new Option();
+	  for (i=1;i<=31;i++){
+	    selector.options[i] = new Option(i,i);
+	  }
+	}
+
+	function addYears(selector){
+		var i;
+		var j=2017;
+	  selector.options[0] = new Option();
+	  for (i=1;i<=108;i++){
+	    selector.options[i] = new Option(j,j);
+	    j--;
+	  }
+	}
+	//usage:
+	addDays(document.getElementById("jour"));
+	addYears(document.getElementById("annee"));
+
+
+});
